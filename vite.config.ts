6ff61216eb9a7d48c49905db953131cec4ce5e63
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-08-21 15:43:21 +0200

    Add Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     environment: "jsdom",
     setupFiles: ["./src/test-setup.ts"],
